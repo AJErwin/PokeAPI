c42293da622e7afeb108c041a4a0e0d2b71133cd
Alien 9 <Alien 9@192.167.1.47>
2026-02-19 12:03:52 -0600

    GetAll

--- src/main/java/PokeApi/Programacion/Controller/PokemonController.java
+++ src/main/java/PokeApi/Programacion/Controller/PokemonController.java
@@ -1,23 +1,29 @@
 package PokeApi.Programacion.Controller;
 
 import PokeApi.Programacion.ML.Pokemon;
-import PokeApi.Programacion.ML.PokemonListResponse;
 import PokeApi.Programacion.Service.PokemonService;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class PokemonController {
 
     @Autowired
     private PokemonService pokemonService;
 
-    @GetMapping("/api/pokemon/all")
-    public List<Pokemon> getAll(@RequestParam(defaultValue = "20") int limit,
-            @RequestParam(defaultValue = "0") int offset) {
-        return pokemonService.GetAll(limit, offset);
+    @GetMapping("/pokedex")
+    public String getAll(@RequestParam(defaultValue = "20") int limit,
+                         @RequestParam(defaultValue = "0") int offset, 
+                         Model model) {
+        
+        List<Pokemon> pokemones = pokemonService.GetAll(limit, offset);
+        
+        model.addAttribute("pokemones", pokemones);
+        
+        return "index";
     }
 }
